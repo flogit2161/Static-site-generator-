80e776f5e20ce843e15dded0d4948fb96b42b9ba
flogit2161 <flo12gab@gmail.com>
2025-10-24 03:02:07 +0200

    Added LeafNode class and its method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,5 +19,19 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
     
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value to convert to HTML")
+        if self.tag is None:
+            return self.value
+        props_str = self.props_to_html()
+        start_tag = f"<{self.tag}{props_str}>"
+        end_tag = f"</{self.tag}>"
+        return f"{start_tag}{self.value}{end_tag}"
+        
 
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
-
+from htmlnode import LeafNode
 class HTMLNodeTest(unittest.TestCase):
     def test_eq(self):
         node = HTMLNode(tag="div", value="Hello", props={"class": "greeting"})
@@ -10,6 +10,9 @@ class HTMLNodeTest(unittest.TestCase):
         self.assertEqual(node.props, {"class": "greeting"})
     # Need to add more tests here
 
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
 if __name__ == "__main__":
     unittest.main()
